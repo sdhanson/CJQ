67e7c5bc09a6da8c7189fcbf855513c3b615bdd5
sdhanson <sdhanson@usc.edu>
2018-06-03 15:07:57 -0500

    Adding about page. Fixed footer... still have random design and accesibility things to fix across the board

---
+++ about.js
@@ -0,0 +1,11 @@
+// $(window).resize(function () {
+//   var viewportWidth = $(window).width();
+  // if (viewportWidth < 600) {
+  //         $("img").removeClass("pull-right").addClass("center-block");
+  //         $("h1").style("color", "yellow");
+  // }
+  // else if(viewportWidth >= 600) {
+  //   $("img").removeClass("center-block").addClass("pull-right");
+  // }
+// });
+
